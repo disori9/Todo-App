a38b6a74778b6cc23cb3f4f5b12fb39c49b9ac37
Ori <disori777@gmail.com>
2024-10-22 15:50:16 +0800

    Implement edit function

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
 import functions
 import PySimpleGUI as sg
 
+from functions import write_file
+
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(key="todo")
 add_button = sg.Button("Add")
-list_box = sg.LBox(values=functions.get_file(), key="todos",
-                   enable_events=True, size=(35,10))
+list_box = sg.Listbox(values=functions.get_file(), key="todos",
+                   enable_events=True, size=(44,10))
 edit_button = sg.Button("Edit")
 
 layout = [[label],
@@ -18,13 +20,25 @@ window = sg.Window('My To-Do App', layout, font=('Verdana', 12))
 # values return a dictionary type consisting of what was inputted in the input box
 while True:
     event, values = window.read()
-
+    print(event)
+    print(values)
     match event:
         case "Add":
             todos = functions.get_file()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_file(todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+            todos = functions.get_file()
+
+            for index, todo in enumerate(todos):
+                if todo_to_edit == todo:
+                    todos[index] = new_todo
+
+            write_file(todos)
+
         case sg.WINDOW_CLOSED:
             break
 window.close()
