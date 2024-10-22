cb9929e5859f2a1ad91e6e4925d3c16fbd81aea5
Ori <disori777@gmail.com>
2024-10-22 15:26:51 +0800

    Decouple layout

--- gui.py
+++ gui.py
@@ -5,10 +5,9 @@ label = sg.Text("Type in a todo")
 input_box = sg.InputText(key="todo")
 add_button = sg.Button("Add")
 
+layout = [[label], [input_box, add_button]]
 
-window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
-                   font=('Verdana', 12))
+window = sg.Window('My To-Do App', layout, font=('Verdana', 12))
 
 # event returns button label (because clicking button is an event,
 # values return a dictionary type consisting of what was inputted in the input box
