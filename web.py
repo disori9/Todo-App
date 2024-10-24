4cfe5ae3943c4690e375b3d36fa98f36b72d03a8
Ori <disori777@gmail.com>
2024-10-25 00:56:19 +0800

    Implement complete todo feature in checkbox

--- web.py
+++ web.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import functions
 
+todos = functions.get_file()
 def add_todo():
      input_todo = st.session_state['new_todo'] + '\n'
-     print(input_todo)
-     todos_local = functions.get_file()
-     todos_local.append(input_todo)
-     functions.write_file(todos_local)
+     todos.append(input_todo)
+     functions.write_file(todos)
+
 
 st.title("My Todo App")
 st.subheader("These are my todos")
-todos = functions.get_file()
 
-for todo in todos:
-     st.checkbox(todo)
+for index, todo in enumerate(todos):
+     todo_checkbox = st.checkbox(todo, key=todo)
+     if todo_checkbox is True:
+          todos.pop(index)
+          functions.write_file(todos)
+          del st.session_state[todo]
+          st.experimental_rerun()
 
 st.text_input(label="", placeholder="Enter a todo:",
               key="new_todo", on_change=add_todo)
 
-
+st.session_state
